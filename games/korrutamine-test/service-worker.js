ed945fd88f6f12d006431063869ad785cd5a6e5b
vaeducat-maker <va.educat@gmail.com>
2026-07-28 16:53:56 +0300

    ok

--- games/korrutamine-test/service-worker.js
+++ games/korrutamine-test/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='edukass-korrutustabel-v61-kuus-northern-fjord';
-const ASSETS=['./','./index.html','./game.css?v=61','./game.js?v=61','./manifest.webmanifest','./locales/et.js?v=61','./chapter-one.config.js?v=61','./i18n.js?v=61','./question-engine.js?v=61','./progress-store.js?v=61','./assets/icon-192.png?v=61','./assets/icon-512.png?v=61','./assets/share-korrutustabel.png?v=41','./assets/edukass-cat.png','./assets/edukass-cat-transparent.png','./assets/edukass-cat-reward-surprise.png','./assets/edukass-cat-reward-joy.png','./assets/edukass-kolm-world.png','./assets/edukass-world-rocket.png'];
+const CACHE='edukass-korrutustabel-v62-kuus-cosmic-fjord';
+const ASSETS=['./','./index.html','./game.css?v=62','./game.js?v=61','./manifest.webmanifest','./locales/et.js?v=61','./chapter-one.config.js?v=61','./i18n.js?v=61','./question-engine.js?v=61','./progress-store.js?v=61','./assets/icon-192.png?v=61','./assets/icon-512.png?v=61','./assets/share-korrutustabel.png?v=41','./assets/edukass-cat.png','./assets/edukass-cat-transparent.png','./assets/edukass-cat-reward-surprise.png','./assets/edukass-cat-reward-joy.png','./assets/edukass-kolm-world.png','./assets/edukass-world-rocket.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
