516184b2b92d2b03c0a0036e80f23377578d16e6
vaeducat-maker <va.educat@gmail.com>
2026-09-09 17:29:33 +0300

    Refresh Sõnatreener cache for flashcards-only version

--- games/kiire-sonatreener/service-worker.js
+++ games/kiire-sonatreener/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE='edukass-sonatreener-v3';
+const CACHE='edukass-sonatreener-v4';
 const ASSETS=[
   './','./index.html',
   './trainer.css?v=20260909-1','./lesson-library.css?v=20260909-2',
-  './trainer.js?v=20260909-1','./poster-fix.js?v=20260909-7',
+  './trainer.js?v=20260909-2','./poster-fix.js?v=20260909-7',
   './poster-pretty/0.txt?v=20260909-2','./poster-pretty/1.txt?v=20260909-2','./poster-pretty/2.txt?v=20260909-2',
   './poster-pretty/3.txt?v=20260909-2','./poster-pretty/4.txt?v=20260909-2','./poster-pretty/5.txt?v=20260909-2',
   './icon-192.png','./icon-512.png','./icon.svg'
