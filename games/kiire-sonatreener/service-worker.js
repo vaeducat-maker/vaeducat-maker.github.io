e88c776d93a16921290580beee9d230e8cf7911a
vaeducat-maker <va.educat@gmail.com>
2026-09-09 17:56:03 +0300

    Disable Sõnatreener service worker caching

--- games/kiire-sonatreener/service-worker.js
+++ games/kiire-sonatreener/service-worker.js
@@ -1,15 +1,17 @@
-const CACHE='edukass-sonatreener-v6';
-const ASSETS=[
-  './','./index.html',
-  './trainer.css?v=20260909-1','./lesson-library.css?v=20260909-2',
-  './trainer.js?v=20260909-5','./poster-fix.js?v=20260909-9',
-  './poster-pretty/0.txt?v=20260909-2','./poster-pretty/1.txt?v=20260909-2','./poster-pretty/2.txt?v=20260909-2',
-  './poster-pretty/3.txt?v=20260909-2','./poster-pretty/4.txt?v=20260909-2','./poster-pretty/5.txt?v=20260909-2',
-  './poster-pretty.jpg?v=20260909-1','./icon-192.png','./icon-512.png','./icon.svg'
-];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    try{
+      const keys=await caches.keys();
+      await Promise.all(keys.map(k=>caches.delete(k)));
+      await self.registration.unregister();
+      await self.clients.claim();
+    }catch(e){}
+  })());
+});
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+  event.respondWith(fetch(event.request,{cache:'no-store'}));
 });
